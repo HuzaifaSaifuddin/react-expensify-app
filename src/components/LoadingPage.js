88c48e26b8825c79cc50868648e37bf6fade7943
Huzaifa Saifuddin <huzaifa@Huzaifas-MacBook-Pro.local>
2021-02-19 21:51:39 +0530

    Add Expensify styles

---
+++ src/components/LoadingPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const LoadingPage = () => (
+  <div className="loader">
+    <img class="loader__image" src="/images/loader.gif" />
+  </div>
+)
+
+export default LoadingPage;
